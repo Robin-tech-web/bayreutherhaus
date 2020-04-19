6546707f994c16b0d663b75a7454d1d688163b66
Robin Weidlich <robinweidlich7@gmail.com>
2020-04-19 17:57:24 -0300

    add freizeitaktivitäten und öffnungszeiten pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/wildpark'
+  get 'pages/freizeitaktivitaeten'
+  get 'pages/oeffnungszeiten'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
