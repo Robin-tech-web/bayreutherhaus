49344d73f440846ec61fb3395f40846e6285eec5
Robin Weidlich <robinweidlich7@gmail.com>
2020-04-19 18:19:38 -0300

    add remaining pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'pages/wildpark'
   get 'pages/freizeitaktivitaeten'
   get 'pages/oeffnungszeiten'
+  get 'pages/anfahrt'
+  get 'pages/kontakt'
+  get 'pages/galerie'
+  get 'pages/impressum'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
